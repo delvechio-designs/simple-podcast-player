60b3ab898dfbdd764aa182b5e7a662af2f1186fd
delvechio-designs <patrick@delvechiodesigns.com>
2025-06-09 20:05:39 -0700

    Update class-player-render.php

--- public/class-player-render.php
+++ public/class-player-render.php
@@ -95,7 +95,17 @@ class SPP_Render {
             <p  class="spp-sub"><?php echo $sub; ?></p>
 
             <div class="spp-top">
-              <button class="spp-play">▶</button>
+              <button class="spp-play">
+  <!-- play icon -->
+  <svg class="spp-icon spp-play-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+    <path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80L0 432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/>
+  </svg>
+  <!-- pause icon -->
+  <svg class="spp-icon spp-pause-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+    <path d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48h-32z"/>
+  </svg>
+</button>
+
               <div class="spp-wave"></div>
             </div>
 
